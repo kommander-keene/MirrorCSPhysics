78d5150d6d9ad42b395a02c3a050f85bc5d77fac
kommander-keene <69388003+kommander-keene@users.noreply.github.com>
2023-08-25 12:16:17 -0700

    Update and rename Ballz.cs to RegisterNetworkPhysics.cs
    Sorry guys

--- Assets/Scripts/RegisterNetworkPhysics.cs
+++ Assets/Scripts/RegisterNetworkPhysics.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ballz : MonoBehaviour
+public class RegisterNetworkPhysics : MonoBehaviour
 {
     IEnumerator Go()
     {
